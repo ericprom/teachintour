f3b12e2d47bfe1615d8b799f818ec9090e4c2bc9
Surasak Prommarat <prommarats@gmail.com>
2016-04-04 10:48:04 +0700

    update profile form

--- vendor/dektrium/yii2-user/views/settings/account.php
+++ vendor/dektrium/yii2-user/views/settings/account.php
@@ -23,46 +23,49 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <?= $this->render('/_alert', ['module' => Yii::$app->getModule('user')]) ?>
+<section id="hero" class="login">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+          <?= $this->render('_menu') ?>
+      </div>
+      <div class="col-md-9">
+          <div class="panel panel-default">
+              <div class="panel-heading">
+                  <?= Html::encode($this->title) ?>
+              </div>
+              <div class="panel-body">
+                  <?php $form = ActiveForm::begin([
+                      'id'          => 'account-form',
+                      'options'     => ['class' => 'form-horizontal'],
+                      'fieldConfig' => [
+                          'template'     => "{label}\n<div class=\"col-lg-9\">{input}</div>\n<div class=\"col-sm-offset-3 col-lg-9\">{error}\n{hint}</div>",
+                          'labelOptions' => ['class' => 'col-lg-3 control-label'],
+                      ],
+                      'enableAjaxValidation'   => true,
+                      'enableClientValidation' => false,
+                  ]); ?>
 
-<div class="row">
-    <div class="col-md-3">
-        <?= $this->render('_menu') ?>
+                  <?= $form->field($model, 'email') ?>
+
+                  <?= $form->field($model, 'username') ?>
+
+                  <?= $form->field($model, 'new_password')->passwordInput() ?>
+
+                  <hr />
+
+                  <?= $form->field($model, 'current_password')->passwordInput() ?>
+
+                  <div class="form-group">
+                      <div class="col-lg-offset-3 col-lg-9">
+                          <?= Html::submitButton(Yii::t('user', 'Save'), ['class' => 'btn btn-block btn-success']) ?><br>
+                      </div>
+                  </div>
+
+                  <?php ActiveForm::end(); ?>
+              </div>
+          </div>
+      </div>
     </div>
-    <div class="col-md-9">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <?= Html::encode($this->title) ?>
-            </div>
-            <div class="panel-body">
-                <?php $form = ActiveForm::begin([
-                    'id'          => 'account-form',
-                    'options'     => ['class' => 'form-horizontal'],
-                    'fieldConfig' => [
-                        'template'     => "{label}\n<div class=\"col-lg-9\">{input}</div>\n<div class=\"col-sm-offset-3 col-lg-9\">{error}\n{hint}</div>",
-                        'labelOptions' => ['class' => 'col-lg-3 control-label'],
-                    ],
-                    'enableAjaxValidation'   => true,
-                    'enableClientValidation' => false,
-                ]); ?>
-
-                <?= $form->field($model, 'email') ?>
-
-                <?= $form->field($model, 'username') ?>
-
-                <?= $form->field($model, 'new_password')->passwordInput() ?>
-
-                <hr />
-
-                <?= $form->field($model, 'current_password')->passwordInput() ?>
-
-                <div class="form-group">
-                    <div class="col-lg-offset-3 col-lg-9">
-                        <?= Html::submitButton(Yii::t('user', 'Save'), ['class' => 'btn btn-block btn-success']) ?><br>
-                    </div>
-                </div>
-
-                <?php ActiveForm::end(); ?>
-            </div>
-        </div>
-    </div>
-</div>
+  </div>
+</section>
