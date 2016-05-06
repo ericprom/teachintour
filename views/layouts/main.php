30d3f317b8e0a0907360cc3e48aeae05326c5c65
Surasak Prommarat <prommarats@gmail.com>
2016-05-06 21:14:15 +0700

    no message

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -80,7 +80,7 @@ $user = Yii::$app->user->identity;
       <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a>.</p>
     <![endif]-->
 
-    <div id="preloader">
+    <!-- <div id="preloader">
       <div class="sk-spinner sk-spinner-wave">
         <div class="sk-rect1"></div>
         <div class="sk-rect2"></div>
@@ -88,7 +88,7 @@ $user = Yii::$app->user->identity;
         <div class="sk-rect4"></div>
         <div class="sk-rect5"></div>
       </div>
-    </div>
+    </div> -->
     <!-- End Preload -->
 
     <div class="layer"></div>
