b416fb5a2dd77707b8d510d91c5714e12dfe7619
Surasak Prommarat <prommarats@gmail.com>
2016-05-17 16:16:47 +0700

    adding rbac

--- config/web.php
+++ config/web.php
@@ -48,6 +48,9 @@ $config = [
         ],
     ],
     'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'ri,iy9oN0yomiNd6]',
