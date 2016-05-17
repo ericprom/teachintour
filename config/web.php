652bf79280bef5197c8365decac2298bf3038461
Surasak Prommarat <prommarats@gmail.com>
2016-05-17 16:21:07 +0700

    no message

--- config/web.php
+++ config/web.php
@@ -48,9 +48,6 @@ $config = [
         ],
     ],
     'components' => [
-        'authManager' => [
-            'class' => 'yii\rbac\DbManager',
-        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'ri,iy9oN0yomiNd6]',
