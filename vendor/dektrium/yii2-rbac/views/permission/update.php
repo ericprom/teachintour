bd398c978da67cd749e5b544522878f0f687ae05
Surasak Prommarat <prommarats@gmail.com>
2016-05-26 18:15:19 +0700

    no message

---
+++ vendor/dektrium/yii2-rbac/views/permission/update.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Dektrium project.
+ *
+ * (c) Dektrium project <http://github.com/dektrium>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @var $model dektrium\rbac\models\Role
+ * @var $this  yii\web\View
+ */
+
+$this->title = Yii::t('rbac', 'Update permission');
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+<section id="content">
+  <div class="content-wrap">
+    <div class="container clearfix">
+      <?php $this->beginContent('@dektrium/rbac/views/layout.php') ?>
+
+      <?= $this->render('_form', [
+          'model' => $model,
+      ]) ?>
+
+      <?php $this->endContent() ?>
+    </div>
+  </div>
+</section>
