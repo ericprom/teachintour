9cfb6f9173e5048e3684eaabe88ebd79ebdd415c
Surasak Prommarat <prommarats@gmail.com>
2016-06-23 11:02:04 +0700

    update terms

--- modules/terms/views/default/index.php
+++ modules/terms/views/default/index.php
@@ -19,26 +19,26 @@ $this->title = 'Terms | '.Yii::$app->params["company_name"].'';
             The terms and conditions are agreement between you and teachin’ tour. We recommended you to read them carefully. By using our site, you confirm that you accept these terms of use and that you agree to comply with them. If you have any further question or concern please feel free to contact us.
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>APPLICATION AND ACCEPTANCE PROCEDURE</strong></div>
+            <div><strong>1. APPLICATION AND ACCEPTANCE PROCEDURE</strong></div>
             <div style="text-indent: 20px;">
               Each participant must first complete and submit the teachin’ tour application. There is no commitment in submitting an application as each applicant must first be accepted to participate on teachin’ tour program, within 3-5 business days of receiving an application, teachin’ tour will contact applicant in regard to a) ACCEPT INTO THE PROGRAM, b) NON-ACCEPT and C) WE NEEDED MORE INFORMATION. If accept teachin’ tour will notify applicant by email or phone with the instructions for the next stage of process.
             </div>
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>ORIENTATION</strong></div>
+            <div><strong>2. ORIENTATION</strong></div>
             <div>
               <div style="text-indent: 20px;">Participants on teachin’ tour program will have orientation meeting in head home stay to start participant’s volunteer conducted by teachin’ tour, these orientation meeting are designed to help prepare participant for the volunteer project.
               </div>
             </div>
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>VOLUNTEER PROJECT</strong></div>
+            <div><strong>3. VOLUNTEER PROJECT</strong></div>
             <div style="text-indent: 20px;">Participants are signed to volunteer project regard to your duration choosing. A project summary is emailed to participant at that time. A more detailed project on project’s goal, host family background, accommodation, volunteer tasks, suggested living in the local area, safety consideration and much more.</div>
             <div style="text-indent: 20px;">(e.g.) teachin’ tour required the volunteer to teach the minimum 3-5 hours a week or more than that depend on the volunteer at the school that we have provided. B). Moreover teaching the volunteer will have free time so you may go wherever you needed.</div>
 
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>SUPPORTS AND SERVICES</strong></div>
+            <div><strong>4. SUPPORTS AND SERVICES</strong></div>
             <div style="text-indent: 20px;">After confirmed and the completed payment, you will be a volunteer of teachin’ tour program.</div>
             <div style="text-indent: 20px;">Accommodations are provided by us as the following;</div>
             <div style="margin-left: 20px;text-indent: 20px;">A. Free home stay, you may be required to share bedroom and bathroom.</div>
@@ -51,34 +51,34 @@ $this->title = 'Terms | '.Yii::$app->params["company_name"].'';
             <div style="margin-left: 50px;text-indent: 20px;">- We also provided individual a local host family, stay over with local family live like they are. Study more Thai local ISAN culture.</div>
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>PAYMENT/FEE</strong></div>
+            <div><strong>5. PAYMENT/FEE</strong></div>
             <div style="text-indent: 20px;">Full payments for the The teachin’ tour program is required to confirm your arrival booking date and the schedule in a minimum 5-7 days after submitted the application form.</div>
             <div style="text-indent: 20px;">We had 3 channels for arranging the payment, please inform us which way you prefer to do after you confirmed and completed booking date arrival. We will send you any link of payment. If you pay by credit card it would be automatic deducted.</div>
             <div style="text-indent: 20px;">Method of payment; </div>
-            <div style="text-indent: 20px;">- Credit card</div>
-            <div style="text-indent: 20px;">- Money Gram/Western Union is accepted. (If you prefer this way please advise us to send you the information)</div>
-            <div style="text-indent: 20px;">- ATM transfers, ATM transfer is available if you are somewhere in Thailand</div>
+            <div style="text-indent: 20px;">5.1 Credit card</div>
+            <div style="text-indent: 20px;">5.2 Money Gram/Western Union is accepted. (If you prefer this way please advise us to send you the information)</div>
+            <div style="text-indent: 20px;">5.3 ATM transfers, ATM transfer is available if you are somewhere in Thailand</div>
             <div style="margin-left: 20px;text-indent: 20px;">- Fee of program depends on your duration.</div>
             <div style="margin-left: 20px;text-indent: 20px;">If there is any problem with credit or debit card that mean we cannot collect the payment we will not allow you to complete the application and will not collect any money. In the event that we have problem processing your credit card after booking is made we will notify you to provide alternative payment.</div>
             <div style="margin-left: 20px;text-indent: 20px;">- We will send you a primary invoice by email immediately after the payment is received.</div>
 
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>START AND CHANGE DATE</strong></div>
+            <div><strong>6. START AND CHANGE DATE</strong></div>
             <div style="text-indent: 20px;">After confirmed and completed the full payment the teachin’ tour program. You may change date of your placement by up to 1- 3 months from the original date, if you wish to do so please kindly email us about the reason and give us the prefer of alternative date. For the first changing date, there is no additional cost.  But if there is change is the second time we will require you to pay an administration USD30 for each change.</div>
 
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>CACELLATION/REFUND POLICY</strong></div>
+            <div><strong>7. CACELLATION/REFUND POLICY</strong></div>
             <div style="margin-left: 20px;">Cancellation of program participation must be writing.</div>
-            <div style="text-indent: 20px;"><strong>Cancellation by you</strong></div>
+            <div style="text-indent: 20px;"><strong>7.1 Cancellation by you</strong></div>
             <div style="margin-left: 20px;text-indent: 20px;">If there is any cancel by you with any reason before started date and no show up for the program the payment is non-refundable.</div>
-            <div style="text-indent: 20px;"><strong>Cancellation by teacin’ tour</strong></div>
+            <div style="text-indent: 20px;"><strong>7.2 Cancellation by teacin’ tour</strong></div>
             <div style="margin-left: 20px;text-indent: 20px;">If there are any unexpected situations in any reason such as political, airline schedule change, nature disaster, or any damage to accommodation.  We will inform you and make you an alternative itinerary of choose an alternative depart date. If any changes are not possible for you, we will refund a full payment back to you.</div>
             <div style="margin-left: 20px;text-indent: 20px;">In case of after arrival and made it home stay, you feel do not like and wanted to cancel you will receive 30% of the amount of payment.</div>
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>TRAVELING</strong></div>
+            <div><strong>8. TRAVELING</strong></div>
             <div style="text-indent: 20px;">The applicant is responsible for arranging and paying for their own travel, specifically included airfare and other transportation costs.</div>
             <div style="text-indent: 20px;">Air port picks up please Following SUPPORTS AND SERVICES</div>
             <div style="text-indent: 20px;">The applicants supposed to carry all necessary documents when you traveling abroad.</div>
@@ -86,30 +86,30 @@ $this->title = 'Terms | '.Yii::$app->params["company_name"].'';
             <div style="text-indent: 20px;"> The applicants are responsible for ensuring their travel documents are up to date and valid date.</div>
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>DISRUPTION / UNEXPECTED DURING THE PROGRAM</strong></div>
+            <div><strong>9. DISRUPTION / UNEXPECTED DURING THE PROGRAM</strong></div>
             <div style="text-indent: 20px;">If there is any disrupted in any situations, the volunteer should inform us via email or if is urgent you should make a call or contact the local coordinator. We will investigate the situation and contact you immediately.</div>
             <div style="text-indent: 20px;">If the situation serious enough to the significant affect your program. We will try our best to arrange the suitable alternative as soon as possible.</div>
             <div style="text-indent: 20px;">We do not make any refund in these situations and you will be responsible for any travel cost involve.</div>
             <div style="text-indent: 20px;">We do not accept any responsibility for any expense, loss, damage or claim.</div>
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>DRUG USE</strong></div>
+            <div><strong>10. DRUG USE</strong></div>
             <div style="text-indent: 20px;">Drug use (Unless prescribed by a doctor) is strictly prohibited and will result in instant dismissal from the program with no reasonable.
 teachin’ tour reserves the right to refuse acceptance to any applicant.</div>
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>INSURANCE</strong></div>
+            <div><strong>11. INSURANCE</strong></div>
             <div style="text-indent: 20px;">All applicant/volunteer on teachin’ tour program are expected to arrange their own comprehensive travel insurance.</div>
             <div style="text-indent: 20px;">We do not provide the applicant/volunteer with any kind of insurance.</div>
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>PASSPORT/VISA</strong></div>
+            <div><strong>12. PASSPORT/VISA</strong></div>
             <div style="text-indent: 20px;">You must ensure that your passport should be valid for at least 6 months beyond your date of entry into Thailand.</div>
             <div style="text-indent: 20px;">You must ensure that all travel documents are valid. We cannot accept any responsibility if entry to Thailand is denied.</div>
             <div style="text-indent: 20px;">teachin’ tour advice volunteers to enter on tourist visa. You are not permitted to work on tourist visas, because you are not getting paid.</div>
           </div>
           <div style="margin-top: 10px;">
-            <div><strong>GENERAL/VOLUNTEER AGREEMENT</strong></div>
+            <div><strong>13. GENERAL/VOLUNTEER AGREEMENT</strong></div>
             <div style="text-indent: 20px;">When you arrival to head home stay we require you to sign for a volunteer agreement between you and teachin’ tour.</div>
             <div style="text-indent: 20px;">We require for a copy of your passport and others certificate that you needed to show us about your experiences.</div>
           </div>
