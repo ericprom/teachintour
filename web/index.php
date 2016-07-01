6f6635d5a3f16e41c1ad199f68b96ab2f5008e45
Surasak Prommarat <prommarats@gmail.com>
2016-07-01 12:30:28 +0700

    update Omise

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', false);
-defined('YII_ENV') or define('YII_ENV', 'production');
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
 define('OMISE_API_VERSION', '2015-11-17');
 define('OMISE_PUBLIC_KEY', 'pkey_test_54dtg5n2fpnnzpm0l5v');
 define('OMISE_SECRET_KEY', 'skey_test_54dtg5n2erz1nf6424q');
